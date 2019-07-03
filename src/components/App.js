5bfc4067d70410f101aa1576814393bc2d7c4ac0
rafiswe <rafikulislam521@gmail.com>
2019-07-04 00:53:21 +0600

    Start a new react project name hello-react

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import First from './FirstCom/First'
+
+function App() {
+  return (
+    <div className="App">
+        <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FirstCom/First.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class First extends Component{
+    render(){
+        return (
+            <h1>Hello Rafi, I am your first component.</h1>
+        ) 
+    }
+    
+}
+
+export default First
