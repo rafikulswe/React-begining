159aada36bc9ad5d69e8c1718a68add98276a45b
rafiswe <rafikulislam521@gmail.com>
2019-07-16 02:04:06 +0600

    event handler details completed

--- src/components/FirstCom/EventHandler.js
+++ src/components/FirstCom/EventHandler.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react'
 
 class EventHandler extends Component{
+    state = {
+        username: ''
+    }
     clickHandler = (event) =>{
-        console.log("You Click Me");
+        this.setState({
+            username: event.target.value
+        })
     }
     render(){
         return(
@@ -10,10 +15,11 @@ class EventHandler extends Component{
                 <form action="#">
                     <div className="form-group">
                         <label htmlFor="username">Name</label>
-                        <input type="text" id="username" onClick={this.clickHandler}/>
+                        <input type="text" id="username" onChange={this.clickHandler}/>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
+                {this.state.username ? <p>Hi Mr. {this.state.username}</p> : ""}
             </div>
         )
     }
