e98b80eedf3696d0a93f2be628ca00c640b8a79c
rafiswe <rafikulislam521@gmail.com>
2019-07-25 01:38:18 +0600

    style css in react again

--- src/components/FirstCom/DataList.js
+++ src/components/FirstCom/DataList.js
@@ -3,8 +3,16 @@ import '../App.css';
 
 class DataList extends Component{
     render(){
+        let stylecss = {
+            border: '1px solid black',
+            height: '300px',
+            width: '600px',
+            background: 'gray',
+            padding: '10px 10px',
+            margin: '0 auto'
+        }
         return(
-            <div className="main-class">
+            <div className="main-class" style={stylecss}>
                 <li>React Redux Full Course</li>
             </div>
         )
